bff5c77b14e6590a09d2a282de4bca0f1aedc643
Upendra rajput <upendrarajput@gmail.com>
2022-11-14 14:50:22 +0530

    use-error-boundary

--- writty-app/src/index.js
+++ writty-app/src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from "react-dom";
 import {BrowserRouter} from "react-router-dom"
 import APP from "./component/app";
 import "./styles/style.scss"
+import ErrorBoundary from "./component/use-error-boundary"
 ReactDOM.render(
     <BrowserRouter>
+    <ErrorBoundary>
     <APP/>
+    </ErrorBoundary>
     </BrowserRouter>,document.getElementById("root")
 )
